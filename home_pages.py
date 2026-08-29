0900023a5d21e6042ed52e0083a5ba969cd57873
aurlaw0809 <aurlaw0809@highgateschool.org.uk>
2026-08-29 19:13:29 +0100

    super cool beats

--- home_pages.py
+++ home_pages.py
@@ -48,7 +48,10 @@ BUTTON_SPACING = 10
 #-----------------------------------------------------------------------------------------------------------------------------------
 #SOUND SET UP
 
-pygame.mixer.music.load("assets/music/steven_universe.mp3")
+bg_music = ["assets/music/lease_frutiger_aero.mp3", "assets/music/dont_stop_till_you_get_enough.mp3"]
+bg_index = random.randint(0, len(bg_music) - 1)
+
+pygame.mixer.music.load(bg_music[bg_index])
 pygame.mixer.music.set_volume(NORMAL_MUSIC_VOLUME)
 
 CLICK_SOUND = pygame.mixer.Sound("assets/sound_effects/click.wav")
